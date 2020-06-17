84e1b9a488923218e636201aa9879eb836f3aa49
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-06-17 10:19:50 -0700

    stub for search, not done. Merge works for 0, 1, 2 item arrays

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,31 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    first = 0
+    last = (len(arr) - 1)
+
+    found = False
+    found_index = -1
+
+    # if first <= last and not found:
+    #     return 
+    while first <= last and not found:
+        middle = (first + last) // 2
+
+        if arr[middle] == target:
+            found_index = middle
+            found = True
+            print("Found")
+            return found_index
+
+        else: 
+            if target < arr[middle]:
+                last = middle - 1
+            else:
+                first = middle + 1
+    # return found_index
+    print("NOT Found")
+    return -1
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -11,4 +36,7 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
+    pass
 
+my_list = [1, 2, 3, 4, 5]
+print(binary_search(my_list, 2, 0, len(my_list) - 1))
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,5 +1,6 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
+    # 
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
 
@@ -12,8 +13,36 @@ def merge(arrA, arrB):
 def merge_sort(arr):
     # Your code here
 
+    # check if empty or if only one thing in array
+    if len(arr) == 0 or len(arr) == 1:
+        print("empty or 1 item array")
+        return arr
+
+    # check if only 2 things in array
+    if len(arr) == 2:
+        if arr[0] < arr[1]:
+            print("left side was smaller (2 items)")
+            return arr
+        else:
+            temp = arr[1]
+            arr[1] = arr[0]
+            arr[0] = temp
+            print("right side was smaller, or identical (2 items)")
+            return arr
+
+    # 3, 6, 8, 2, 5, 4, 1
+    # [3, 6, 8] [2] [5, 4, 1]
+    # middle
+    middle = len(arr) // 2 
+    # left 
+    left = arr[:middle]
+    right = arr[middle:]
+    # right
 
     return arr
+
+my_list = []
+print(merge_sort(my_list))
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
